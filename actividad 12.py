9e0276602c31844e79ad993af203c37ccd010cab
Usuario <hugocanizzabala@gmail.com>
2025-08-07 22:45:54 -0600

    repartidor3

--- actividad 12.py
+++ actividad 12.py
@@ -1,51 +1,49 @@
-
 def quick_sort(lista):
     if len(lista) <= 1:
         return lista
     else:
         pivote = lista[0]
-        menores = [x for x in lista[1:] if x < pivote]
-        iguales = [x for x in lista if x == pivote]
-        mayores = [x for x in lista[1:] if x > pivote]
+        menores = [x for x in lista[1:] if x[1]['paquetes'] < pivote[1]['paquetes']]
+        iguales = [x for x in lista if x[1]['paquetes'] == pivote[1]['paquetes']]
+        mayores = [x for x in lista[1:] if x[1]['paquetes'] > pivote[1]['paquetes']]
         return quick_sort(menores) + iguales + quick_sort(mayores)
+
 
 repartidor = {}
 cantidad = int(input("¿Cuántos repartidores desea ingresar? "))
 
+
 for i in range(cantidad):
-    print(f"\n Repartidor #{i + 1}")
+    print(f"\nRepartidor #{i + 1}")
     empleado = input("Ingrese el número de empleado: ")
     nombre = input("Ingrese el nombre completo: ")
     zona = int(input("Ingrese zona: "))
-    paquetes= int(input("Ingrese paquetes entregados: "))
+    paquetes = int(input("Ingrese paquetes entregados: "))
 
     repartidor[empleado] = {
         "nombre": nombre,
         "zona": zona,
         "paquetes": paquetes,
-
-
     }
 
-print("ordenar empleado por paquetes entregados")
-paq_ordenado=quick_sort(list(repartidor.items()))
-for empleado in paq_ordenado:
-    datos = empleado[empleado]
-    print(f"\nempleado: {empleado}")
+
+print("\n--- Empleados ordenados por paquetes entregados ---")
+paq_ordenado = quick_sort(list(repartidor.items()))
+for codigo, datos in paq_ordenado:
+    print(f"\nCódigo de empleado: {codigo}")
     print(f"Nombre: {datos['nombre']}")
-    print(f"zona: {datos['zona']}")
-    print(f"paquete: {datos['paquete']}")
+    print(f"Zona: {datos['zona']}")
+    print(f"Paquetes entregados: {datos['paquetes']}")
 
 
-print("\nBúsqueda de empleado")
-buscado = input("Ingrese codigo de empleado: ")
+print("\n--- Búsqueda de empleado ---")
+buscado = input("Ingrese código de empleado: ")
 
 if buscado in repartidor:
     emp = repartidor[buscado]
-    print("\nrepartidor:")
+    print(f"\nEmpleado encontrado:")
     print(f"Nombre: {emp['nombre']}")
-    print(f"zona: {emp['zona']}")
-    print(f"paquete: {emp['paquete']}")
-
+    print(f"Zona: {emp['zona']}")
+    print(f"Paquetes entregados: {emp['paquetes']}")
 else:
-    print("repartidor no encontrado.")
+    print("Repartidor no encontrado.")
